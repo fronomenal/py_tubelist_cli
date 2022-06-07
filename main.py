bee8ee61c463df567a9d201c6f3538ee33a5aa7b
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-06-07 23:41:48 +0100

    environment variables setup

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+from dotenv import load_dotenv
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def init():
+    load_dotenv("./secrets/.env")
 
 
-# Press the green button in the gutter to run the script.
+def print_hi():
+    print(os.environ["YT_API_KEY"])
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    init()
+    print_hi()
